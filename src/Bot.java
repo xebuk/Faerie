d0a4d936c3c3fdb0736f948464ba7e38f7e5b824
xebuk <sozdykov2017@yandex.ru>
2024-09-10 12:39:47 +0500

    Created Initial Structure

---
+++ src/Bot.java
@@ -0,0 +1,5 @@
+public class Bot {
+    public void run() {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Bot botyara = new Bot();
+        botyara.run();
+    }
+}
