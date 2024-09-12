70bdd558efad2e2f60041844b8ad6f64fb7e351a
xebuk <sozdykov2017@yandex.ru>
2024-09-12 17:29:10 +0500

    Changed access level for readToken function and changed from Paths.get to Path.of

--- src/main/java/TokenReader.java
+++ src/main/java/TokenReader.java
@@ -1,11 +1,9 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class TokenReader {
-    public static String readToken() throws IOException {
-        Path tokenFilePath = Paths.get("../token_dir/token.txt");
-        return Files.readString(tokenFilePath).trim();
+    protected static String readToken() throws IOException {
+        return Files.readString(Path.of("../token_dir/token.txt")).trim();
     }
 }
