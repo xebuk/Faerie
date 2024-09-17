ca0cb44406573e459c8dd339040a2fbeb004dc01
xebuk <sozdykov2017@yandex.ru>
2024-09-17 12:32:41 +0500

    Optimised Ability_Bot Class and renamed Ability_Bot to AbilBot

--- src/main/java/AbilBot.java
+++ src/main/java/AbilBot.java
@@ -1,24 +1,18 @@
 import org.telegram.telegrambots.abilitybots.api.objects.*;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.abilitybots.api.bot.AbilityBot;
-import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
-import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 import static org.telegram.telegrambots.abilitybots.api.objects.Locality.*;
 import static org.telegram.telegrambots.abilitybots.api.objects.Privacy.*;
-import static org.telegram.telegrambots.abilitybots.api.util.AbilityUtils.getChatId;
 
-public class Ability_Bot extends AbilityBot implements LongPollingSingleThreadUpdateConsumer {
+public class AbilBot extends AbilityBot {
     boolean reverse = false;
     private final OkHttpTelegramClient telegramClient = new OkHttpTelegramClient(TokenReader.readToken());
 
-    public Ability_Bot() throws IOException {
+    public AbilBot() throws IOException {
         super(new OkHttpTelegramClient(TokenReader.readToken()), "Faerie");
         super.onRegister();
     }
@@ -60,25 +54,5 @@ public class Ability_Bot extends AbilityBot implements LongPollingSingleThreadUp
                 .privacy(PUBLIC)
                 .action(reversal)
                 .build();
-    }
-
-    @Override
-    public void consume(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText() && reverse) {
-            String chatId = update.getMessage().getChatId().toString();
-            String msg = update.getMessage().getText();
-
-            String reversedMsg = new StringBuilder(msg).reverse().toString();
-
-            SendMessage sendMessage = new SendMessage(chatId, reversedMsg);
-            System.out.println("Received \"" + msg + "\" from " + chatId);
-            System.out.println("Sent \"" + reversedMsg + "\" to " + chatId);
-
-            try {
-                telegramClient.execute(sendMessage);
-            } catch (TelegramApiException e) {
-                throw new RuntimeException(e);
-            }
-        }
     }
 }
