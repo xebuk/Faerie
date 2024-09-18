99a2b5b1db55dc7062156102346412080724d8ee
xebuk <sozdykov2017@yandex.ru>
2024-09-18 21:44:15 +0500

    Added searchArticleId Function

--- src/main/java/TokenReader.java
+++ src/main/java/TokenReader.java
@@ -1,6 +1,9 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
 
 public class TokenReader {
     public static String readToken() throws IOException {
@@ -10,6 +13,24 @@ public class TokenReader {
 
     protected static int readCreatorId() throws IOException {
         Path creatorID_FilePath = Path.of("../token_dir/creatorID.txt");
-        return Integer.parseInt(Files.readString(creatorID_FilePath).trim());
+        return parseInt(Files.readString(creatorID_FilePath).trim());
+    }
+
+    protected static String searchArticleId(String section, String name) throws IOException {
+        Path articleID_FilePath = Path.of("../token_dir/" + section + ".txt");
+        List<String> lines;
+        for (int i = 0; i < 300; i++) {
+            lines = Files.readAllLines(articleID_FilePath);
+            String[] sep;
+            for (String articleId: lines) {
+                sep = articleId.trim().split("~ ");
+                if (sep[1].toLowerCase().contains(name.toLowerCase())) {
+                    //System.out.println(sep[0]);
+                    //System.out.println(sep[1]);
+                    return sep[0];
+                }
+            }
+        }
+        return "1";
     }
 }
