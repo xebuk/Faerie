a294dbe6a2ceef6f41e1700aefe24db92eb449db
xebuk <sozdykov2017@yandex.ru>
2024-09-19 11:29:51 +0500

    Created Constants Interface

---
+++ src/main/java/Constants.java
@@ -0,0 +1,11 @@
+public interface Constants {
+    String helpMessage = """
+            /help - выводит список команд
+            /search [раздел (spells/items/bestiary)] [название] - выводит искомую статью
+            /roll [кол-во костей] [кол-во сторон] - выводит значение костей
+            """;
+    String spells = "Fireball!";
+    String items = "Deсk of many things!";
+    String bestiary = "Tarrasque!";
+    String searchMessage = "What would you like to search?";
+}
