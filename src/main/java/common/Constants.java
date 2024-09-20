3aa04ac8fe79ebb5d681d9fd3788540614f2b174
Larts <alex1790007@mail.ru>
2024-09-20 20:33:06 +0500

    Added Drawer's image output path

--- src/main/java/common/Constants.java
+++ src/main/java/common/Constants.java
@@ -33,4 +33,6 @@ public interface Constants {
     String SPELLS = "Fireball!";
     String ITEMS = "Deck of many things!";
     String BESTIARY = "Tarrasque!";
+
+    String IMAGE_OUTPUT_PATH = ".";
 }
