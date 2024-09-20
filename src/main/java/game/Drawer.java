8f41ccf4e3707a419e6a2f0c8d13af83de2ad2fb
Larts <alex1790007@mail.ru>
2024-09-20 20:33:31 +0500

    Now supports many drawing sessions

--- src/main/java/game/Drawer.java
+++ src/main/java/game/Drawer.java
@@ -1,11 +1,16 @@
 package game;
 
+import common.Constants;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.List;
 
 public class Drawer {
     private static final int CANVAS_WIDTH = 720;  //1440;
@@ -15,10 +20,11 @@ public class Drawer {
     private static final double FOCAL_LENGTH = 1.0 / Math.tan(FOV / 2);
     private static final double ASPECT_RATIO = (double) CANVAS_WIDTH / CANVAS_HEIGHT;
 
-//    private static final double NEAR_PLANE = 0.1;      // Clipping plane
-
     private double cameraX, cameraY, cameraZ;
     private double[][] depthBuffer;
+
+    private Graphics2D g2d;
+    private BufferedImage image;
 
     public Drawer(double cameraX, double cameraY, double cameraZ) {
         this.cameraX = cameraX;
@@ -27,6 +33,21 @@ public class Drawer {
         this.depthBuffer = new double[CANVAS_WIDTH][CANVAS_HEIGHT];
 
         resetDepthBuffer();
+    }
+
+    public void startDrawing() {
+        image = new BufferedImage(CANVAS_WIDTH, CANVAS_HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        g2d = image.createGraphics();
+    }
+
+    public void endDrawing() {
+        g2d.dispose();
+        try {
+            String path = Paths.get(Constants.IMAGE_OUTPUT_PATH, "output.png").toString();
+            ImageIO.write(image, "png", new File(path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void resetDepthBuffer() {
@@ -121,14 +142,14 @@ public class Drawer {
         }
     }
 
-    public void drawScene(Graphics2D g2d, Cube[] cubes) {
+    public void drawScene(List<Cube> cubes) {
         resetDepthBuffer();
         for (Cube cube : cubes) {
             drawCube(g2d, cube);
         }
     }
 
-    private void fillBackground(Graphics2D g2d, Color color) {
+    public void fillBackground(Color color) {
         int[] xBackground = {0, CANVAS_WIDTH, CANVAS_WIDTH, 0};
         int[] yBackground = {0, 0, CANVAS_HEIGHT, CANVAS_HEIGHT};
         g2d.setColor(color);
@@ -178,24 +199,17 @@ public class Drawer {
 
         Random random = new Random();
         int cubeCountTest = 10;
-        Cube[] cubes = new Cube[cubeCountTest];
+        List<Cube> cubes = new ArrayList<>();
         for (int i = 0; i < cubeCountTest; i++) {
-            cubes[i] = new Cube(random.nextInt(10)-5, random.nextInt(10)-5, random.nextInt(10)-5, 1);
+            cubes.add(new Cube(random.nextInt(10)-5, random.nextInt(10)-5, random.nextInt(10)-5, 1));
         }
 
-        try {
-            BufferedImage image = new BufferedImage(CANVAS_WIDTH, CANVAS_HEIGHT, BufferedImage.TYPE_INT_ARGB);
-            Graphics2D g2d = image.createGraphics();
+        drawer.startDrawing();
 
-            drawer.fillBackground(g2d, Color.BLACK);
+        drawer.fillBackground(Color.BLACK);
 
-            drawer.drawScene(g2d, cubes);
+        drawer.drawScene(cubes);
 
-            g2d.dispose();
-
-            ImageIO.write(image, "png", new File("output.png"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        drawer.endDrawing();
     }
 }
