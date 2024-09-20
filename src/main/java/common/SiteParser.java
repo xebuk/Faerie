e8cc6192f27b8c0e8a5a04ce7d25124ffc6a52c5
xebuk <sozdykov2017@yandex.ru>
2024-09-21 04:23:32 +0500

    Created functions to do searching in site's other sections

--- src/main/java/common/SiteParser.java
+++ src/main/java/common/SiteParser.java
@@ -57,20 +57,130 @@ public class SiteParser {
         return result;
     }
 
+    public static ArrayList<String> RacesGrabber(String id) {
+        String article;
+        try {
+            article = DataReader.searchArticleId("race", id);
+        } catch (IOException e) {
+            article = id;
+        }
+
+        Connection link = Jsoup.connect(URL + "race" + "/" + article);
+        Document page;
+        try {
+            page = link.get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Elements name = page.select("h2.card-title[itemprop=name]");
+        Elements source = page.select("ul.params.card__article-body");
+
+        Elements body = page.select("div.desc.card__article-body[itemprop=articleBody]");
+        Elements p = body.select("p,h3.underlined");
+
+        ArrayList<String> result = new ArrayList<>();
+        result.add(name.text());
+        result.add(source.text() + "\n");
+
+        //System.out.println(name.text());
+        //System.out.println(source.text());
+        for (Element i: p) {
+            //System.out.println(i.text());
+            result.add(i.text() + "\n");
+        }
+
+        result.add("Информация взята с " + URL + "race" + "/" + article);
+        return result;
+    }
+
+    public static ArrayList<String> FeatsGrabber(String id) {
+        String article;
+        try {
+            article = DataReader.searchArticleId("feats", id);
+        } catch (IOException e) {
+            article = id;
+        }
+
+        Connection link = Jsoup.connect(URL + "feats" + "/" + article);
+        Document page;
+        try {
+            page = link.get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Elements name = page.select("h2.card-title[itemprop=name]");
+        Elements body = page.select("ul.params.card__article-body");
+
+        Elements li = body.select("li:not(.subsection.desc)");
+        Elements liDescBody = body.select("li.subsection.desc").select("p");
+
+        ArrayList<String> result = new ArrayList<>();
+        result.add(name.text());
+        result.add(liDescBody.text());
+
+        for (Element i : li) {
+            //System.out.println(i.text());
+            result.add("  -  " + i.text() + "\n");
+        }
+        result.add("Информация взята с " + URL + "feats" + "/" + article);
+        return result;
+    }
+
+    public static ArrayList<String> BackgroundsGrabber(String id) {
+        String article;
+        try {
+            article = DataReader.searchArticleId("backgrounds", id);
+        } catch (IOException e) {
+            article = id;
+        }
+
+        Connection link = Jsoup.connect(URL + "backgrounds" + "/" + article);
+        Document page;
+        try {
+            page = link.get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Elements name = page.select("h2.card-title[itemprop=name]");
+        Elements body = page.select("div[itemprop=description]");
+
+        Elements descBody = body.select("p,h3.smallSectionTitle,div.table-wrapper");
+
+        ArrayList<String> result = new ArrayList<>();
+        result.add(name.text());
+
+        for (Element i: descBody) {
+            result.add(i.text() + "\n");
+        }
+        result.add("Информация взята с " + URL + "feats" + "/" + article);
+        return result;
+    }
+
     public static void DictWriter(String section) {
         Connection link;
         Document page = null;
         //ArrayList<String> data = new ArrayList<>();
         //data.add(" ");
 
-        for (int i = 1; i < 401; i++) { // 401 - временное число, вскоре заменю на 1601
+        for (int i = 700; i <= 1000; i++) {
             Elements name = null;
+            Elements check1;
+            Elements check2;
             boolean pageNotFound = false;
             do {
                 link = Jsoup.connect(URL + section + "/" + i);
                 try {
                     page = link.get();
                 } catch (IOException e) {
+                    pageNotFound = true;
+                    break;
+                }
+
+                check1 = page.select("div");
+                if (check1.hasClass("private-card") || check1.hasClass("card__group-homebrew")) {
                     pageNotFound = true;
                     break;
                 }
@@ -82,9 +192,7 @@ public class SiteParser {
                 continue;
             }
 
-            //data.add("\"" + name.text() + "\"");
             System.out.println(i + "~ " + name.text());
-
         }
         //System.out.println(data);
     }
