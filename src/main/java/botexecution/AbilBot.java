8e2b41a571323a0ad5fb3366d19016cd92049e22
xebuk <sozdykov2017@yandex.ru>
2024-09-21 20:45:21 +0500

    Created a searchEngine Function

--- src/main/java/botexecution/AbilBot.java
+++ src/main/java/botexecution/AbilBot.java
@@ -14,13 +14,15 @@ import java.util.ArrayList;
 import java.util.Objects;
 import java.util.function.Consumer;
 
-import static common.Constants.URL;
+import static java.lang.System.exit;
 import static org.telegram.telegrambots.abilitybots.api.objects.Locality.*;
 import static org.telegram.telegrambots.abilitybots.api.objects.Privacy.*;
 import static org.telegram.telegrambots.abilitybots.api.util.AbilityUtils.getChatId;
 
 public class AbilBot extends AbilityBot {
     public String sectionId = "";
+    public boolean searchSuccess = false;
+    public String title = "";
 
     public AbilBot() throws IOException {
         super(new OkHttpTelegramClient(DataReader.readToken()), "Faerie");
@@ -38,6 +40,48 @@ public class AbilBot extends AbilityBot {
         return id;
     }
 
+    public boolean searchEngine(String section, String entry, Update update) {
+        ArrayList<String> matches;
+        boolean gotId = false;
+        try {
+            matches = DataReader.searchArticleIds(section, entry);
+        } catch (IOException e) {
+            matches = new ArrayList<>();
+            matches.add("1");
+        }
+
+        if (matches.size() == 1 && Objects.equals(matches.get(0), "1")) {
+            ArrayList<String> article;
+            switch (section) {
+                case "spells", "items", "bestiary":
+                    article = SiteParser.SpellsItemsBestiaryGrabber(section, matches.get(0));
+                    break;
+                case "races":
+                    article = SiteParser.RacesGrabber(matches.get(0));
+                    break;
+                case "feats":
+                    article = SiteParser.FeatsGrabber(matches.get(0));
+                    break;
+                case "backgrounds":
+                    article = SiteParser.BackgroundsGrabber(matches.get(0));
+                    break;
+                default:
+                    return false;
+            }
+
+            articleMessaging(article, update);
+            return false;
+        }
+
+        else {
+            String searchPage = SiteParser.addressWriter(matches, section);
+            SendMessage page = new SendMessage(getChatId(update).toString(), searchPage);
+            page.setParseMode("HTML");
+            silent.execute(page);
+            return true;
+        }
+    }
+
     public void sendList(Update update) {
         SendMessage list = new SendMessage(getChatId(update).toString(), Constants.CLASSES_LIST);
         list.setParseMode("HTML");
@@ -46,25 +90,25 @@ public class AbilBot extends AbilityBot {
 
     public void generateKeyboard(MessageContext ctx) {
         SendMessage gen = new SendMessage(ctx.chatId().toString(), Constants.START_MESSAGE);
-        gen.setReplyMarkup(KeyboardFactory.setOfCommands());
+        gen.setReplyMarkup(KeyboardFactory.setOfCommandsBoard());
         silent.execute(gen);
     }
 
     public void search(MessageContext ctx) {
         SendMessage search = new SendMessage(ctx.chatId().toString(), Constants.SEARCH_MESSAGE);
-        search.setReplyMarkup(KeyboardFactory.searchEngine());
+        search.setReplyMarkup(KeyboardFactory.searchBoard());
         silent.execute(search);
     }
 
     public void roll(MessageContext ctx) {
         SendMessage roll = new SendMessage(ctx.chatId().toString(), Constants.ROLL_MESSAGE);
-        roll.setReplyMarkup(KeyboardFactory.rollVariants());
+        roll.setReplyMarkup(KeyboardFactory.rollVariantsBoard());
         silent.execute(roll);
     }
 
     public void rollAdvantage(Update update) {
         SendMessage rollAdv = new SendMessage(getChatId(update).toString(), Constants.ROLL_MESSAGE_ADVANTAGE);
-        rollAdv.setReplyMarkup(KeyboardFactory.rollAdvantage());
+        rollAdv.setReplyMarkup(KeyboardFactory.rollAdvantageBoard());
         silent.execute(rollAdv);
     }
 
@@ -227,24 +271,32 @@ public class AbilBot extends AbilityBot {
         }
 
         if (update.hasMessage() && update.getMessage().hasText() && !update.getMessage().isCommand()) {
-            if (!sectionId.isEmpty()) {
-                if (sectionId.equals("spells") || sectionId.equals("items") || sectionId.equals("bestiary")) {
-                    articleMessaging(SiteParser.SpellsItemsBestiaryGrabber(sectionId, update.getMessage().getText()), update);
+            if (searchSuccess) {
+                title = update.getMessage().getText();
+                switch (sectionId) {
+                    case "spells", "items", "bestiary":
+                        articleMessaging(SiteParser.SpellsItemsBestiaryGrabber(sectionId, title), update);
+                        break;
+                    case "race":
+                        articleMessaging(SiteParser.RacesGrabber(title), update);
+                        break;
+                    case "feats":
+                        articleMessaging(SiteParser.FeatsGrabber(title), update);
+                        break;
+                    case "backgrounds":
+                        articleMessaging(SiteParser.BackgroundsGrabber(title), update);
+                        break;
+                    default:
+                        searchSuccess = false;
+                        break;
                 }
-
-                else if (sectionId.equals("race")) {
-                    articleMessaging(SiteParser.RacesGrabber(update.getMessage().getText()), update);
-                }
-
-                else if (sectionId.equals("feats")) {
-                    articleMessaging(SiteParser.FeatsGrabber(update.getMessage().getText()), update);
-                }
-
-                else if (sectionId.equals("backgrounds")) {
-                    articleMessaging(SiteParser.BackgroundsGrabber(update.getMessage().getText()), update);
-                }
-
                 sectionId = "";
+                searchSuccess = false;
+                title = "";
+            }
+
+            if (!sectionId.isEmpty()) {
+                searchSuccess = searchEngine(sectionId, update.getMessage().getText(), update);
             }
         }
     }
