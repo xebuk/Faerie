c1cb268dc1c1a92f46ad785f6711a1db470fea72
xebuk <sozdykov2017@yandex.ru>
2024-09-22 12:50:13 +0500

    Created Battle Class

---
+++ src/main/java/game/states/Battle.java
@@ -0,0 +1,14 @@
+package game.states;
+
+import game.entities.NonPlayerCharacter;
+
+public class Battle {
+    public PlayerCharacter MC;
+    public NonPlayerCharacter Enemy;
+    public boolean PlayerTurn;
+
+    Battle(PlayerCharacter MC, NonPlayerCharacter Enemy) {
+        this.MC = MC;
+        this.Enemy = Enemy;
+    }
+}
