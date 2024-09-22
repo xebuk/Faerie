58cd67b769dc16eebe76cc3a0d5bc7a50c03415b
xebuk <sozdykov2017@yandex.ru>
2024-09-22 12:50:59 +0500

    Created NonPlayerCharacter Class

---
+++ src/main/java/game/entities/NonPlayerCharacter.java
@@ -0,0 +1,11 @@
+package game.entities;
+
+import game.characteristics.Types;
+
+public class NonPlayerCharacter {
+    public String name;
+    public Types type;
+    public int health;
+    public int armorClass;
+    public int attackPower;
+}
