dbe0f8104721f7a22cdfa61ccf55cd6350681206
xebuk <sozdykov2017@yandex.ru>
2024-09-22 12:52:41 +0500

    Moved PlayerCharacter and added stats

--- src/main/java/game/PlayerCharacter.java
+++
@@ -1,7 +0,0 @@
-package game;
-
-public class PlayerCharacter {
-    public int health;
-    public int armorClass;
-    public int attackPower;
-}
---
+++ src/main/java/game/entities/PlayerCharacter.java
@@ -0,0 +1,21 @@
+package game.entities;
+
+import game.characteristics.Jobs;
+import game.characteristics.Races;
+
+public class PlayerCharacter {
+    public String name;
+    public Jobs job;
+    public Races race;
+
+    public int health;
+    public int armorClass;
+    public int attackPower;
+
+    public int strength;
+    public int dexterity;
+    public int constitution;
+    public int intelligence;
+    public int wisdom;
+    public int charisma;
+}
