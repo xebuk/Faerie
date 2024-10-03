ca72aa3dee387884272274ab95ece7f8ee20822f
xebuk <sozdykov2017@yandex.ru>
2024-10-03 22:57:28 +0500

    Added a setter Functions

--- src/main/java/game/entities/PlayerCharacter.java
+++ src/main/java/game/entities/PlayerCharacter.java
@@ -21,9 +21,51 @@ public class PlayerCharacter implements Serializable {
     public int wisdom;
     public int charisma;
 
+    public PlayerCharacter() {}
+
     public PlayerCharacter(String name, String job, String race) {
         this.name = name;
         this.job = job;
         this.race = race;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setArmorClass(int armorClass) {
+        this.armorClass = armorClass;
+    }
+
+    public void setAttackPower(int attackPower) {
+        this.attackPower = attackPower;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public void setConstitution(int constitution) {
+        this.constitution = constitution;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
+    }
+
+    public void setCharisma(int charisma) {
+        this.charisma = charisma;
     }
 }
