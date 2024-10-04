28b9dda33c68174f9cdb16809b6e0b1e575d5308
xebuk <sozdykov2017@yandex.ru>
2024-10-04 22:42:27 +0500

    Added safety precautions for chatFile creation

--- src/main/java/botexecution/ChatSession.java
+++ src/main/java/botexecution/ChatSession.java
@@ -1,25 +1,24 @@
 package botexecution;
 
+import common.UserDataHandler;
 import game.entities.PlayerCharacter;
 import org.telegram.telegrambots.abilitybots.api.objects.MessageContext;
 import org.telegram.telegrambots.abilitybots.api.util.AbilityUtils;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashSet;
 
-import static org.telegram.telegrambots.abilitybots.api.util.AbilityUtils.getChatId;
-
 public class ChatSession implements Serializable {
     private final Long chatId;
-    private final File userFile;
+
     public String sectionId = "";
     public boolean searchSuccess = false;
     public String title = "";
 
     public boolean rollCustom = false;
+    public HashSet<String> dicePresets;
 
     public boolean creationOfPc = false;
     public HashSet<String> statProgress = new HashSet<>();
@@ -28,12 +27,12 @@ public class ChatSession implements Serializable {
 
     public ChatSession(Update update) {
         this.chatId = AbilityUtils.getChatId(update);
-        this.userFile = new File("../token_dir/userData/" + chatId);
+        UserDataHandler.createChatFile(String.valueOf(chatId));
     }
 
     public ChatSession(MessageContext ctx) {
         this.chatId = ctx.chatId();
-        this.userFile = new File("../token_dir/userData/" + chatId);;
+        UserDataHandler.createChatFile(String.valueOf(chatId));
     }
 
     public Long getChatId() {
