a5678a4ae992f87489daa76c080fa02a3a466c7c
xebuk <sozdykov2017@yandex.ru>
2024-10-04 14:46:02 +0500

    Changed from Jobs to Job

---
+++ src/main/java/game/characteristics/Job.java
@@ -0,0 +1,9 @@
+package game.characteristics;
+
+import java.io.Serializable;
+
+public class Job implements Serializable {
+    public int startHp;
+    public int startArmorClass;
+    public String startAttackRoll;
+}
