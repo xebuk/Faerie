7126144b6dd47300c0eb4cf659b2909118754af4
xebuk <sozdykov2017@yandex.ru>
2024-10-07 14:13:31 +0500

    Created Artefact class

---
+++ src/main/java/game/items/Artefact.java
@@ -0,0 +1,16 @@
+package game.items;
+
+import java.io.Serializable;
+
+public class Artefact implements Serializable {
+    public int strengthValue;
+    public int dexterityValue;
+    public int constitutionValue;
+    public int intelligenceValue;
+    public int wisdomValue;
+    public int charismaValue;
+
+    public void specialEffect() {
+
+    }
+}
