28d0247ea8a550b81160e1565c897dccbcc230d3
xebuk <sozdykov2017@yandex.ru>
2024-10-07 21:31:00 +0500

    Upgraded Job Class

--- src/main/java/game/characteristics/jobs/Mage.java
+++ src/main/java/game/characteristics/jobs/Mage.java
@@ -1,13 +1,16 @@
 package game.characteristics.jobs;
 
 import game.characteristics.Job;
+import game.items.armors.Robe;
+import game.items.weapons.Staff;
 
 public class Mage extends Job {
 
     public Mage() {
         this.jobName = "Маг";
+        this.weaponType = new Staff();
+        this.armorType = new Robe();
+
         this.startHealth = 6;
-        this.startArmorClass = 6;
-        this.startAttackDice = "1d10";
     }
 }
--- src/main/java/game/characteristics/jobs/Rogue.java
+++ src/main/java/game/characteristics/jobs/Rogue.java
@@ -1,13 +1,16 @@
 package game.characteristics.jobs;
 
 import game.characteristics.Job;
+import game.items.armors.LightArmor;
+import game.items.weapons.Dagger;
 
 public class Rogue extends Job {
 
     public Rogue() {
         this.jobName = "Плут";
+        this.weaponType = new Dagger();
+        this.armorType = new LightArmor();
+
         this.startHealth = 8;
-        this.startArmorClass = 8;
-        this.startAttackDice = "1d12";
     }
 }
