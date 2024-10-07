d4d8e198e1fea982235e68e244e9e67e7d5ab19d
xebuk <sozdykov2017@yandex.ru>
2024-10-07 21:29:56 +0500

    Created CommonItem class

---
+++ src/main/java/game/items/artefacts/CommonItem.java
@@ -0,0 +1,15 @@
+package game.items.artefacts;
+
+import game.items.Artefact;
+
+public class CommonItem extends Artefact {
+
+    public CommonItem() {
+        this.strengthValue = 0;
+        this.dexterityValue = 0;
+        this.constitutionValue = 0;
+        this.intelligenceValue = 0;
+        this.wisdomValue = 0;
+        this.charismaValue = 0;
+    }
+}
