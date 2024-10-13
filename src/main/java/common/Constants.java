84dcfa3d705437fa4e7aa43e69dca7eacbd7b7f2
xebuk <sozdykov2017@yandex.ru>
2024-10-13 17:08:56 +0500

    Added constants

--- src/main/java/common/Constants.java
+++ src/main/java/common/Constants.java
@@ -26,6 +26,7 @@ public interface Constants {
     String SEARCH_MESSAGE_BACKGROUNDS = "Введите название предыстории.";
 
     String SEARCH_MESSAGE_FAIL = "Такой статьи не было найдено. Поиск отменен.";
+    String SEARCH_MESSAGE_FAIL_SECOND_STAGE = "В запросе есть ошибка. Пожалуйста, попробуйте ввести его ещё раз.";
     String SEARCH_MESSAGE_INCORRECT = "Название введено неверно. Поиск отменен.";
     String SEARCH_MESSAGE_IMPOSSIBLE = "Там, где ошибка невозможна, случилась ошибка. Сообщите об этом авторам бота.";
 
@@ -66,6 +67,7 @@ public interface Constants {
     String CUSTOM_DICE = "Uh-oh, big number incoming!";
     String CUSTOM_DICE_MESSAGE = "Введите количество и номер кости в виде: 10d40";
     String CUSTOM_DICE_MESSAGE_WITH_PRESETS = "Выберите вариант из предложенных или введите количество и номер кости в виде: 10d40";
+    String CUSTOM_DICE_ERROR = "Во время вашего броска возникла ошибка. Попробуйте ещё раз.";
 //    String CUSTOM_DICE_MESSAGE = "Write a number and a dice in pattern: 10d40";
 //    String CUSTOM_DICE_MESSAGE_WITH_PRESETS = "Choose a variant below or write a number and a dice in pattern: 10d40";
 
