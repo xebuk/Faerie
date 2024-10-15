828041da407b85f8d7695c5b3115315314c74a3f
xebuk <sozdykov2017@yandex.ru>
2024-10-15 23:54:07 +0500

    Added part of characteristics from character list (DnD)

--- src/main/java/dnd/PlayerDnD.java
+++ src/main/java/dnd/PlayerDnD.java
@@ -1,5 +1,79 @@
 package dnd;
 
-public class PlayerDnD {
+import java.util.ArrayList;
 
+public class PlayerDnD {
+    public String name;
+    public String job;
+    public String background;
+    public String playerName;
+
+    public String race;
+    public String alignment;
+    public int experience;
+    public int level;
+
+    public int inspiration;
+    public int bonusMastery;
+
+    public int armorClass;
+    public int initiativeBonus;
+    public int speed;
+
+    public int healthPoints;
+    public int maxHealthPoints;
+    public int temporaryHealthPoints;
+    public String healthDice;
+    public int deathThrowsSuccess;
+    public int deathThrowsFailure;
+
+    public ArrayList<String> personality;
+    public ArrayList<String> ideals;
+    public ArrayList<String> bonds;
+    public ArrayList<String> flaws;
+
+    public int strength;
+    public int strengthModifier;
+    public int strengthSaveThrow;
+    public int dexterity;
+    public int dexterityModifier;
+    public int dexteritySaveThrow;
+    public int constitution;
+    public int constitutionModifier;
+    public int constitutionSaveThrow;
+    public int intelligence;
+    public int intelligenceModifier;
+    public int intelligenceSaveThrow;
+    public int wisdom;
+    public int wisdomModifier;
+    public int wisdomSaveThrow;
+    public int charisma;
+    public int charismaModifier;
+    public int charismaSaveThrow;
+
+    public int acrobatics;
+    public int analysis;
+    public int athletics;
+    public int perception;
+    public int survival;
+    public int performance;
+    public int intimidation;
+    public int history;
+    public int sleightOfHand;
+    public int arcane;
+    public int medicine;
+    public int deception;
+    public int nature;
+    public int insight;
+    public int religion;
+    public int stealth;
+    public int persuasion;
+    public int animalHandling;
+
+    public int passivePerception;
+
+    public ArrayList<String> attacksAndSpells;
+    public ArrayList<String> equipment;
+    public ArrayList<String> proficienciesAndLanguages;
+    public ArrayList<String> traits;
 }
