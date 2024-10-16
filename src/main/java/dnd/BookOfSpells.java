85baf5aa057930dd8b4b929fe8aeb8f2bcac73e4
xebuk <sozdykov2017@yandex.ru>
2024-10-16 13:11:17 +0500

    Created BookOfSpells Class

---
+++ src/main/java/dnd/BookOfSpells.java
@@ -0,0 +1,43 @@
+package dnd;
+
+import java.util.ArrayList;
+
+public class BookOfSpells {
+    public ArrayList<String> cantrips;
+
+    public ArrayList<String> level1;
+    public int level1SpellSlotsTotal;
+    public int level1SpellSlotsSpent;
+
+    public ArrayList<String> level2;
+    public int level2SpellSlotsTotal;
+    public int level2SpellSlotsSpent;
+
+    public ArrayList<String> level3;
+    public int level3SpellSlotsTotal;
+    public int level3SpellSlotsSpent;
+
+    public ArrayList<String> level4;
+    public int level4SpellSlotsTotal;
+    public int level4SpellSlotsSpent;
+
+    public ArrayList<String> level5;
+    public int level5SpellSlotsTotal;
+    public int level5SpellSlotsSpent;
+
+    public ArrayList<String> level6;
+    public int level6SpellSlotsTotal;
+    public int level6SpellSlotsSpent;
+
+    public ArrayList<String> level7;
+    public int level7SpellSlotsTotal;
+    public int level7SpellSlotsSpent;
+
+    public ArrayList<String> level8;
+    public int level8SpellSlotsTotal;
+    public int level8SpellSlotsSpent;
+
+    public ArrayList<String> level9;
+    public int level9SpellSlotsTotal;
+    public int level9SpellSlotsSpent;
+}
