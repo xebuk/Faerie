c3a71c9c628ae8ccea028a3383f027c4017ba99b
xebuk <sozdykov2017@yandex.ru>
2024-10-16 21:34:17 +0500

    Added more ArrayLists for information managing

--- src/main/java/dnd/DungeonMasterDnD.java
+++ src/main/java/dnd/DungeonMasterDnD.java
@@ -1,10 +1,21 @@
 package dnd;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DungeonMasterDnD implements Serializable {
     public String username;
 
     public HashMap<String, PlayerDnD> playerDnDHashMap;
+
+    public ArrayList<CommonItemDnD> itemCollection;
+    public ArrayList<MagicItemDnD> magicItemCollection;
+
+    public ArrayList<QuestDnD> questRoster;
+
+    public ArrayList<NonPlayerDnD> npcRoster;
+    public ArrayList<CompanionDnD> companionRoster;
+
+    public HashMap<String, String> settingNotes;
 }
