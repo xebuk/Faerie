90ebf23dec8b45b8c2b624fe3ca1c6ff58e80fa5
xebuk <sozdykov2017@yandex.ru>
2024-10-16 15:09:55 +0500

    Added saver and reader for articleIds as HashMap

--- src/main/java/common/DataReader.java
+++ src/main/java/common/DataReader.java
@@ -1,10 +1,10 @@
 package common;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static java.lang.Integer.parseInt;
@@ -52,6 +52,47 @@ public class DataReader {
         }
 
         return results;
+    }
+
+    public static void saveArticleIdsAsHashMap(String section) throws IOException {
+        HashMap<Integer, String> articleIds = new HashMap<>();
+
+        Path articleIdsFilePath = Path.of("../token_dir/searchID/" + section + ".txt");
+        List<String> lines = Files.readAllLines(articleIdsFilePath);
+        String[] separated;
+
+        File articleIdsHash = new File("../token_dir/searchIDasHashMaps/" + section + ".txt");
+        articleIdsHash.createNewFile();
+
+        for (String articleId: lines) {
+            separated = articleId.trim().split("~ ");
+            articleIds.put(Integer.parseInt(separated[0]), separated[1]);
+        }
+
+        FileOutputStream out = new FileOutputStream(articleIdsHash);
+        ObjectOutputStream output = new ObjectOutputStream(out);
+        output.writeObject(articleIds);
+
+        output.close();
+        out.close();
+    }
+
+    public static HashMap<Integer, String> readArticleIds(String section) {
+        File articleIdsHash = new File("../token_dir/searchIDasHashMaps/" + section + ".txt");
+
+        FileInputStream in;
+        ObjectInputStream input;
+
+        try {
+            in = new FileInputStream(articleIdsHash);
+            input = new ObjectInputStream(in);
+            HashMap<Integer, String> articleIds = (HashMap<Integer, String>) input.readObject();
+            input.close();
+            in.close();
+            return articleIds;
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static File frame = new File(Constants.IMAGE_OUTPUT_PATH + "output.png");
