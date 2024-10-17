58d03311ddabd2917bcdba15eefabf8cfa896101
xebuk <sozdykov2017@yandex.ru>
2024-10-17 20:13:21 +0500

    Added more variables

--- src/main/java/dnd/characteristics/JobDnD.java
+++ src/main/java/dnd/characteristics/JobDnD.java
@@ -1,7 +1,20 @@
 package dnd.characteristics;
 
-public class JobDnD {
-    public String name;
-    public boolean usesMagic;
+import dnd.dmtools.MagicItemDnD;
 
+import java.util.ArrayList;
+
+public class JobDnD {
+    public String title;
+    public boolean usesMagic;
+    public String healthDice;
+
+    public String mainStat1;
+    public String mainStat2;
+
+    public String mainSkill1;
+    public String mainSkill2;
+    public String mainSkill3;
+
+    public ArrayList<AbilityDnD> abilityRoster;
 }
