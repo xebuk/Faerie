d011cce5b4e7c9d997bfb9f2d51f0516cf797dfa
xebuk <sozdykov2017@yandex.ru>
2024-10-18 12:24:38 +0500

    Added DM's username and chatId and initialised all objects

--- src/main/java/dnd/DungeonMasterDnD.java
+++ src/main/java/dnd/DungeonMasterDnD.java
@@ -7,17 +7,22 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DungeonMasterDnD implements Serializable {
+    public long chatId;
     public String username;
+    public String campaignName;
 
-    public HashMap<String, PlayerDnD> playerDnDHashMap;
+    public HashMap<String, PlayerDnD> playerDnDHashMap = new HashMap<>();
+    public int multiclassLimit = 0;
 
-    public ArrayList<CommonItemDnD> itemCollection;
-    public ArrayList<MagicItemDnD> magicItemCollection;
+    public ArrayList<CommonItemDnD> itemCollection = new ArrayList<>();
+    public ArrayList<MagicItemDnD> magicItemCollection = new ArrayList<>();
 
-    public ArrayList<QuestDnDForDm> questRoster;
+    public ArrayList<QuestDnDForDm> questRoster = new ArrayList<>();
 
-    public ArrayList<NonPlayerDnD> npcRoster;
-    public ArrayList<CompanionDnD> companionRoster;
+    public ArrayList<NonPlayerDnD> npcRoster = new ArrayList<>();
+    public ArrayList<CompanionDnD> companionRoster = new ArrayList<>();
 
-    public HashMap<String, String> settingNotes;
+    public HashMap<String, String> settingNotes = new HashMap<>();
+
+    public DungeonMasterDnD() {}
 }
