e138ce322940b878c4ad6d5e84dded6fbf6b88cb
xebuk <sozdykov2017@yandex.ru>
2024-10-20 23:52:31 +0500

    Changed types of ArrayLists from String to AbilityDnD

--- src/main/java/dnd/characteristics/BookOfSpellsDnD.java
+++ src/main/java/dnd/characteristics/BookOfSpellsDnD.java
@@ -7,41 +7,41 @@ public class BookOfSpellsDnD {
     public int saveDc;
     public int attackBonus;
 
-    public ArrayList<String> cantrips;
+    public ArrayList<AbilityDnD> cantrips;
 
-    public ArrayList<String> level1;
+    public ArrayList<AbilityDnD> level1;
     public int level1SpellSlotsTotal;
     public int level1SpellSlotsSpent;
 
-    public ArrayList<String> level2;
+    public ArrayList<AbilityDnD> level2;
     public int level2SpellSlotsTotal;
     public int level2SpellSlotsSpent;
 
-    public ArrayList<String> level3;
+    public ArrayList<AbilityDnD> level3;
     public int level3SpellSlotsTotal;
     public int level3SpellSlotsSpent;
 
-    public ArrayList<String> level4;
+    public ArrayList<AbilityDnD> level4;
     public int level4SpellSlotsTotal;
     public int level4SpellSlotsSpent;
 
-    public ArrayList<String> level5;
+    public ArrayList<AbilityDnD> level5;
     public int level5SpellSlotsTotal;
     public int level5SpellSlotsSpent;
 
-    public ArrayList<String> level6;
+    public ArrayList<AbilityDnD> level6;
     public int level6SpellSlotsTotal;
     public int level6SpellSlotsSpent;
 
-    public ArrayList<String> level7;
+    public ArrayList<AbilityDnD> level7;
     public int level7SpellSlotsTotal;
     public int level7SpellSlotsSpent;
 
-    public ArrayList<String> level8;
+    public ArrayList<AbilityDnD> level8;
     public int level8SpellSlotsTotal;
     public int level8SpellSlotsSpent;
 
-    public ArrayList<String> level9;
+    public ArrayList<AbilityDnD> level9;
     public int level9SpellSlotsTotal;
     public int level9SpellSlotsSpent;
 
