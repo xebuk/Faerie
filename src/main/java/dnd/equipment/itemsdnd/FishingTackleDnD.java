259ba1121182facb9c58282edfd9c06e6ed5bf6c
xebuk <sozdykov2017@yandex.ru>
2024-10-29 15:01:19 +0500

    Created WeaponDnD Class and presets for it, WeaponTraitsDnD Enum and DamageTypeDnD Enum

---
+++ src/main/java/dnd/equipment/itemsdnd/FishingTackleDnD.java
@@ -0,0 +1,10 @@
+package dnd.equipment.itemsdnd;
+
+import dnd.equipment.ItemDnD;
+
+public class FishingTackleDnD extends ItemDnD {
+
+    public FishingTackleDnD() {
+
+    }
+}
---
+++ src/main/java/dnd/equipment/weaponsdnd/GreatswordDnD.java
@@ -0,0 +1,4 @@
+package dnd.equipment.weaponsdnd;
+
+public class GreatswordDnD {
+}
