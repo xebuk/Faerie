09a92d53d31bf8c16bc13e6a03a0f21c0c0925df
xebuk <sozdykov2017@yandex.ru>
2024-11-19 15:24:19 +0500

    Merged all weapons files into two

--- src/main/java/dnd/equipment/MagicWeaponDnD.java
+++ src/main/java/dnd/equipment/MagicWeaponDnD.java
@@ -1,26 +1,27 @@
 package dnd.equipment;
 
+import common.Constants;
+import dnd.values.CurrencyDnD;
 import dnd.values.WeaponTraitsDnD;
 
 import java.util.HashSet;
 
 public class MagicWeaponDnD extends WeaponDnD {
-    public String name;
-    public String summary;
-
-    public int value;
-
-    public int weight;
-
-    public WeaponTraitsDnD type;
-
-    public String attackDice;
-    public String damageType;
-
-    public HashSet<WeaponTraitsDnD> traits;
-    public int minDistance;
-    public int maxDistance;
-
     public int hitBonus;
-    public String specialEffects;
+    public String effects;
+
+    public MagicWeaponDnD() {
+        this.name = "Своё магическое оружие";
+        this.summary = Constants.STANDARD_INVENTORY_SUMMARY;
+
+        this.amountInInstance = 1;
+
+        this.value = 1;
+        this.currencyGrade = CurrencyDnD.GOLD_COINS;
+
+        this.weight = 1;
+
+        this.hitBonus = 1;
+        this.effects = "Введите свои эффекты.";
+    }
 }
--- src/main/java/dnd/equipment/WeaponDnD.java
+++ src/main/java/dnd/equipment/WeaponDnD.java
@@ -5,13 +5,6 @@ import dnd.values.WeaponTraitsDnD;
 import java.util.HashSet;
 
 public class WeaponDnD extends ItemDnD {
-    public String name;
-    public String summary;
-
-    public int value;
-
-    public int weight;
-
     public WeaponTraitsDnD type;
 
     public String attackDice;
@@ -20,4 +13,121 @@ public class WeaponDnD extends ItemDnD {
     public HashSet<WeaponTraitsDnD> traits;
     public int minDistance;
     public int maxDistance;
+
+    public static class BattleaxeDnD {
+    }
+
+    public static class BlowgunDnD {
+    }
+
+    public static class ClubDnD {
+    }
+
+    public static class DaggerDnD {
+    }
+
+    public static class DartDnD {
+    }
+
+    public static class DoubleBladedScimitarDnD {
+    }
+
+    public static class FlailDnD {
+    }
+
+    public static class GlaiveDnD {
+    }
+
+    public static class GreataxeDnD {
+    }
+
+    public static class GreatclubDnD {
+    }
+
+    public static class GreatswordDnD {
+    }
+
+    public static class HalbergDnD {
+    }
+
+    public static class HandaxeDnD {
+    }
+
+    public static class HandCrossbowDnD {
+    }
+
+    public static class HeavyCrossbowDnD {
+    }
+
+    public static class JavelinDnD {
+    }
+
+    public static class LanceDnD {
+    }
+
+    public static class LightCrossbowDnD {
+    }
+
+    public static class LightHammerDnD {
+    }
+
+    public static class LongbowDnD {
+    }
+
+    public static class LongswordDnD {
+    }
+
+    public static class MaceDnD {
+    }
+
+    public static class MaulDnD {
+    }
+
+    public static class MorningstarDnD {
+    }
+
+    public static class NetDnD {
+    }
+
+    public static class PikeDnD {
+    }
+
+    public static class QuarterstaffDnD {
+    }
+
+    public static class RapierDnD {
+    }
+
+    public static class ScimitarDnD {
+    }
+
+    public static class ShortbowDnD {
+    }
+
+    public static class ShortswordDnD {
+    }
+
+    public static class SickleDnD {
+    }
+
+    public static class SlingDnD {
+    }
+
+    public static class SpearDnD {
+    }
+
+    public static class TridentDnD {
+    }
+
+    public static class WarhammerDnD {
+    }
+
+    public static class WarPickDnD {
+    }
+
+    public static class WhipDnD {
+    }
+
+    public static class YklwaDnD {
+    }
 }
