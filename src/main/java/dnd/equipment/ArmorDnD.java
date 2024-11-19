61c606c357b7be4cc1b01d82ad33639e7aa8e8db
xebuk <sozdykov2017@yandex.ru>
2024-11-19 15:33:25 +0500

    Merged all armors files into one

--- src/main/java/dnd/equipment/ArmorDnD.java
+++ src/main/java/dnd/equipment/ArmorDnD.java
@@ -3,13 +3,6 @@ package dnd.equipment;
 import dnd.values.ArmorTypeDnD;
 
 public class ArmorDnD extends ItemDnD {
-    public String name;
-    public String summary;
-
-    public int value;
-
-    public int weight;
-
     public ArmorTypeDnD type;
 
     public int armorClass;
@@ -18,4 +11,62 @@ public class ArmorDnD extends ItemDnD {
     public int strengthRequirement;
 
     public boolean hasStealthDisadvantage;
+
+    public static class BreastplateDnD {
+    }
+
+    public static class ChainMailDnD {
+    }
+
+    public static class ChainShirtDnD {
+    }
+
+    public static class HalfPlateDnD {
+    }
+
+    public static class HideArmorDnD {
+    }
+
+    public static class LeatherArmorDnD {
+    }
+
+    public static class PaddedArmorDnD extends ArmorDnD {
+
+        public PaddedArmorDnD() {
+            this.name = "Стеганный";
+            this.type = ArmorTypeDnD.LIGHT;
+
+            this.value = 5;
+
+            this.armorClass = 11;
+            this.dexterityModMax = 0;
+
+            this.strengthRequirement = 0;
+
+            this.hasStealthDisadvantage = true;
+
+            this.weight = 8;
+        }
+    }
+
+    public static class PlateArmorDnD {
+    }
+
+    public static class RingMailDnD {
+    }
+
+    public static class ScaleMailDnD {
+    }
+
+    public static class ShieldDnD {
+    }
+
+    public static class SpikedArmorDnD {
+    }
+
+    public static class SplintArmorDnD {
+    }
+
+    public static class StuddedLeatherArmorDnD {
+    }
 }
