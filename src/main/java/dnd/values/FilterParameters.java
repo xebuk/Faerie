9ca92226de9316f94a63f961a1c098fe90db198c
xebuk <sozdykov2017@yandex.ru>
2024-12-01 03:11:04 +0500

    Minor changes

---
+++ src/main/java/dnd/values/FilterParameters.java
@@ -0,0 +1,22 @@
+package dnd.values;
+
+import java.io.Serializable;
+
+public enum FilterParameters implements Serializable {
+    GENERAL_SUMMARY,
+    RACE_SUMMARY,
+    JOBS_SUMMARY,
+    BACKGROUND_SUMMARY,
+    CHARACTER_SUMMARY,
+    STATS_SUMMARY,
+    SKILLS_SUMMARY,
+    ABILITIES_SUMMARY,
+    FEATS_SUMMARY,
+    CURRENT_WEAPON,
+    CURRENT_ARMOR,
+    APPEARANCE,
+    INVENTORY_ON_HANDS,
+    INVENTORY_AT_BASE,
+    NOTES,
+    SPELLS;
+}
