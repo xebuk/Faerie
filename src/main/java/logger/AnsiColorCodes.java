12d05d7f18a4386f02fc9da76d9d5b0fde36281b
xebuk <sozdykov2017@yandex.ru>
2024-12-11 21:10:27 +0500

    Added more styles as enums

--- src/main/java/logger/AnsiColorCodes.java
+++ src/main/java/logger/AnsiColorCodes.java
@@ -1,6 +1,7 @@
 package logger;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.logging.Level;
 
 public enum AnsiColorCodes {
@@ -13,24 +14,60 @@ public enum AnsiColorCodes {
     BLUE,
     PURPLE,
     CYAN,
-    WHITE;
+    WHITE,
+
+    BLACK_BOLD,
+    RED_BOLD,
+    GREEN_BOLD,
+    YELLOW_BOLD,
+    BLUE_BOLD,
+    PURPLE_BOLD,
+    CYAN_BOLD,
+    WHITE_BOLD,
+
+    BLACK_HIGH_INTENSITY,
+    RED_HIGH_INTENSITY,
+    GREEN_HIGH_INTENSITY,
+    YELLOW_HIGH_INTENSITY,
+    BLUE_HIGH_INTENSITY,
+    PURPLE_HIGH_INTENSITY,
+    CYAN_HIGH_INTENSITY,
+    WHITE_HIGH_INTENSITY;
 
     private static Map<AnsiColorCodes, String> colors = Map.ofEntries(
             Map.entry(NONE, ""),
-            Map.entry(BLACK, "\u001B[30m"),
-            Map.entry(RED, "\u001B[31m"),
-            Map.entry(GREEN, "\u001B[32m"),
-            Map.entry(YELLOW, "\u001B[33m"),
-            Map.entry(BLUE, "\u001B[34m"),
-            Map.entry(PURPLE, "\u001B[35m"),
-            Map.entry(CYAN, "\u001B[36m"),
-            Map.entry(WHITE, "\u001B[37m")
+            Map.entry(BLACK, "\033[0;30m"),
+            Map.entry(RED, "\033[0;31m"),
+            Map.entry(GREEN, "\033[0;32m"),
+            Map.entry(YELLOW, "\033[0;33m"),
+            Map.entry(BLUE, "\033[0;34m"),
+            Map.entry(PURPLE, "\033[0;35m"),
+            Map.entry(CYAN, "\033[0;36m"),
+            Map.entry(WHITE, "\033[0;37m"),
+
+            Map.entry(BLACK_BOLD, "\033[1;30m"),
+            Map.entry(RED_BOLD, "\033[1;31m"),
+            Map.entry(GREEN_BOLD, "\033[1;32m"),
+            Map.entry(YELLOW_BOLD, "\033[1;33m"),
+            Map.entry(BLUE_BOLD, "\033[1;34m"),
+            Map.entry(PURPLE_BOLD, "\033[1;35m"),
+            Map.entry(CYAN_BOLD, "\033[1;36m"),
+            Map.entry(WHITE_BOLD, "\033[1;37m"),
+
+            Map.entry(BLACK_HIGH_INTENSITY, "\033[0;90m"),
+            Map.entry(RED_HIGH_INTENSITY, "\033[0;91m"),
+            Map.entry(GREEN_HIGH_INTENSITY, "\033[0;92m"),
+            Map.entry(YELLOW_HIGH_INTENSITY, "\033[0;93m"),
+            Map.entry(BLUE_HIGH_INTENSITY, "\033[0;94m"),
+            Map.entry(PURPLE_HIGH_INTENSITY, "\033[0;95m"),
+            Map.entry(CYAN_HIGH_INTENSITY, "\033[0;96m"),
+            Map.entry(WHITE_HIGH_INTENSITY, "\033[0;97m")
     );
 
     private static Map<Level, AnsiColorCodes> consoleColor = Map.of(
-            Level.INFO, CYAN,
-            Level.WARNING, PURPLE,
-            Level.SEVERE, BLACK
+            Level.INFO, CYAN_HIGH_INTENSITY,
+            Level.WARNING, PURPLE_HIGH_INTENSITY,
+            Level.SEVERE, RED_HIGH_INTENSITY
     );
 
     @Override
@@ -40,11 +77,6 @@ public enum AnsiColorCodes {
 
     public static AnsiColorCodes getColorForLevel(Level level) {
         AnsiColorCodes returned = consoleColor.get(level);
-        if (returned == null) {
-            return NONE;
-        }
-        else {
-            return returned;
-        }
+        return Objects.requireNonNullElse(returned, NONE);
     }
 }
